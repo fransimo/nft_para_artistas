b48bfe6462a149d71440c3e15c6db51bed0b3129
Fran Simó <fransimo@gmail.com>
2022-04-24 13:04:06 +0200

    NTFs

--- docs/NFTs/ff.sh
+++ docs/NFTs/ff.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-ff() {
-  sq=$1
-  st=$2
-  end=$3
-  ffmpeg -ss ${st} -to ${end} -i "$HOME/t/2022-04-05 13-01-39.mkv" /home/fran/synced/in_progress/2022_NFT-para-artistas/NFT_frames/${sq}_%05d.png
-}
-
-# -5seg +1seg
-
-# ff 001 01:00 01:03
-ff 003 01:43 01:44
+ffmpeg -ss 00:00:55 -to 00:00:56 -i "$HOME/t/2022-04-05 13-01-39.mkv" /home/fran/synced/in_progress/2022_NFT-para-artistas/NFT_frames/00_01_00_%05d.png
+ffmpeg -ss 00:01:08 -to 00:01:09 -i "$HOME/t/2022-04-05 13-01-39.mkv" /home/fran/synced/in_progress/2022_NFT-para-artistas/NFT_frames/00_01_13_%05d.png
